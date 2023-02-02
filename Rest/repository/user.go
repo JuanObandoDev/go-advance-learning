9e5cb5e8bf69d4ed83fac68445961a9b5f454529
Juan Jose <juanobando.developer@gmail.com>
2023-02-02 13:48:16 -0500

    using repository Design pattern to define insert and get users

---
+++ Rest/repository/user.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/JuanObandoDeveloper/Rest/models"
+)
+
+type UserRespository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (*models.User, error)
+}
+
+var implementation UserRespository
+
+func SetRepository(repo UserRespository) {
+	implementation = repo
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
