b3ab842ce3b6570b543ec13e0598f3a650c404e3
Juan Jose <juanobando.developer@gmail.com>
2023-02-02 13:27:15 -0500

    creating server and start in port 5050

---
+++ Rest/server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port   string
+	JWTKey string
+	DBUrl  string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("port is required")
+	}
+
+	if config.JWTKey == "" {
+		return nil, errors.New("JWTKey is required")
+	}
+
+	if config.DBUrl == "" {
+		return nil, errors.New("DBUrl is required")
+	}
+
+	broker := &Broker{
+		config,
+		mux.NewRouter(),
+	}
+
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Println("Server started on port", b.config.Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
